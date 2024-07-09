1a7e74ed8c9aee9cba7c487e0ccb1b9ce31989a6
ashotisraelyan <ashotisraelyan@gmail.com>
2024-07-09 22:59:00 +0400

    feat: implement user creation

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -6,6 +6,8 @@ import {
 } from '@mui/material';
 import { RouterProvider } from 'react-router-dom';
 import router from './components/Routes';
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from './constants/apollo-client';
 
 const darkTheme = createTheme({
   palette: {
@@ -15,12 +17,14 @@ const darkTheme = createTheme({
 
 const App = () => {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Container>
-        <RouterProvider router={router} />
-      </Container>
-    </ThemeProvider>
+    <ApolloProvider client={apolloClient}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Container>
+          <RouterProvider router={router} />
+        </Container>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 };
 export default App;
