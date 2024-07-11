cbcd85ae8bc8e0bb1b2903c0beb7dadd2276a452
Ashot Israelyan <ashot.israelyan@tumo.org>
2024-07-11 15:04:01 +0400

    feat: create message support

---
+++ backend/src/chats/messages/messages.service.ts
@@ -0,0 +1,30 @@
+import { CreateMessageInput } from './dto/create-message.input';
+import { ChatsRepository } from './../chats.repository';
+import { Injectable } from '@nestjs/common';
+import { Message } from './entities/message.entity';
+import { Types } from 'mongoose';
+
+@Injectable()
+export class MessagesService {
+  constructor(private readonly chatsRepository: ChatsRepository) {}
+
+  async create({ content, chatId }: CreateMessageInput, userId: string) {
+    const message: Message = {
+      content,
+      userId,
+      createdAt: new Date(),
+      _id: new Types.ObjectId(),
+    };
+
+    await this.chatsRepository.findOneAndUpdate(
+      { _id: chatId, $or: [{ userId }, { userIds: { $in: [userId] } }] },
+      {
+        $push: {
+          messages: message,
+        },
+      },
+    );
+
+    return message;
+  }
+}
