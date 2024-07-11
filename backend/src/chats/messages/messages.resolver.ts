ccbb3539d0750da7b7fd3e0a0ed0f88fb243dbdc
Ashot Israelyan <ashot.israelyan@tumo.org>
2024-07-11 17:30:45 +0400

    feat: implement get messages

--- backend/src/chats/messages/messages.resolver.ts
+++ backend/src/chats/messages/messages.resolver.ts
@@ -1,11 +1,12 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MessagesService } from './messages.service';
 import { Message } from './entities/message.entity';
-import { UseGuards } from '@nestjs/common';
 import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
 import { CreateMessageInput } from './dto/create-message.input';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import { TokenPayload } from 'src/auth/token-payload.interface';
+import { UseGuards } from '@nestjs/common';
+import { GetMessagesArgs } from './dto/get-messages.args';
 
 @Resolver(() => Message)
 export class MessagesResolver {
@@ -19,4 +20,8 @@ export class MessagesResolver {
   ) {
     return this.messagesService.create(createMessageInput, user._id);
   }
+
+  @Query(() => [Message], { name: 'messages' })
+  @UseGuards(GqlAuthGuard)
+  async getMessages(@Args() getMessageArgs: GetMessagesArgs) {}
 }
