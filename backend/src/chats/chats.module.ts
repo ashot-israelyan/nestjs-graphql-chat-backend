0909a69cd9345c6784ee00ef348024a1699d1d02
Ashot Israelyan <ashot.israelyan@tumo.org>
2024-07-12 13:07:11 +0400

    feat: refactor resolvers, models

--- backend/src/chats/chats.module.ts
+++ backend/src/chats/chats.module.ts
@@ -2,13 +2,15 @@ import { Module, forwardRef } from '@nestjs/common';
 import { ChatsService } from './chats.service';
 import { ChatsResolver } from './chats.resolver';
 import { DatabaseModule } from '../common/database/database.module';
-import { Chat, ChatSchema } from './entities/chat.entity';
 import { ChatsRepository } from './chats.repository';
 import { MessagesModule } from './messages/messages.module';
+import { ChatDocument, ChatSchema } from './entities/chat.document';
 
 @Module({
   imports: [
-    DatabaseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
+    DatabaseModule.forFeature([
+      { name: ChatDocument.name, schema: ChatSchema },
+    ]),
     forwardRef(() => MessagesModule),
   ],
   providers: [ChatsResolver, ChatsService, ChatsRepository],
